3db06e01736bb7f04b0ed63651a65fdbfe1331d0
giorgiozannini <giorgiozannini97@gmail.com>
2021-02-05 20:36:21 +0100

    more cleaning

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from math import factorial
 from quaternion import Quaternion
 import math
-from copy import deepcopy
+from copy import copy
 
 Q = Quaternion
 
@@ -93,3 +93,137 @@ def initialize_conv(in_channels, out_channels, kernel_size=[2, 2], init_mode="he
     mat += r.float()
 
     return mat
+       
+    
+class QConvAutograd(torch.autograd.Function):
+    
+    @staticmethod
+    def forward(ctx, input, weight, bias, stride, padding, 
+                dilation, groups, type):
+        
+        output = getattr(F, type)(input, weight, bias,
+                                  stride, padding, dilation, groups)
+        
+        ctx.save_for_backward(input, weight, bias)
+        ctx.stride = stride
+        ctx.padding = padding
+        ctx.dilation = dilation
+        ctx.groups = groups
+        
+        return output
+       
+    @staticmethod
+    def backward(ctx, grad_output):
+        
+            
+        input, weight, bias = ctx.saved_tensors
+        stride = ctx.stride
+        padding = ctx.padding 
+        dilation = ctx.dilation
+        groups = ctx.groups
+        
+        grad_input = grad_weight = grad_bias = None   
+        
+        if ctx.needs_input_grad[0]:  
+            grad_input = torch.nn.grad.conv2d_input(
+                        input.shape, weight, grad_output, stride, padding, dilation, groups
+                    )
+            
+            
+        if ctx.needs_input_grad[1]:
+            
+            input[:,input.size(1)//4:] = float("nan")
+            grad_weight = torch.nn.grad.conv2d_weight(
+                        input, weight.shape, grad_output, stride, padding, dilation, groups
+                    ) 
+            
+            a, _b, _c, _d = torch.chunk(grad_weight_r[:,:weight.size(1)//4], 4, 0)
+            grad_weight = torch.cat([torch.cat([  a,   _b,   _c,   _d], 1),
+                                     torch.cat([-_b,    a,   _d,  -_c], 1),
+                                     torch.cat([-_c,  -_d,    a,   _b], 1),
+                                     torch.cat([-_d,   _c,  -_b,    a], 1)], 0)
+        
+        if ctx.needs_input_grad[2] and bias is not None:
+            
+            size = list(range(len(weight.shape)))
+            grad_bias = grad_output.sum([size[0]]+size[2:]).squeeze(0)
+                    
+        return grad_input, grad_weight, grad_bias, None, None, None, None, None
+    
+
+class QTransposeConvAutograd(torch.autograd.Function):
+    
+    @staticmethod
+    def forward(ctx, input, weight, bias, stride, padding, 
+                output_padding, dilation, groups, device, type):
+
+        ctx.save_for_backward(input, weight.a, bias)
+        if bias is not None:
+            bias = bias.to(device)
+            
+        output = getattr(F, type)(input, weight.torch().to(device), bias,
+                                  stride, padding, output_padding, dilation, groups)       
+        return output
+       
+    @staticmethod
+    def backward(ctx, grad_output):
+        
+        grad_input_r = grad_input_bias = None
+        
+        input, weight_r, bias, = ctx.saved_tensors
+        grad_output_r = grad_output[:,:grad_output.size(1)//4]
+        
+        if ctx.needs_input_grad[0] and ctx.needs_input_grad[1]:
+            with torch.enable_grad():
+                
+                grad_input_r = torch.autograd.grad(output, weight_r, grad_output_r)
+            
+            a, _b, _c, _d = torch.chunk(grad_input_r, 0)
+            grad_input = torch.cat([torch.cat([  a,   _b,   _c,   _d], dim=1),
+                                    torch.cat([-_b,    a,   _d,  -_c], dim=1),
+                                    torch.cat([-_c,  -_d,    a,   _b], dim=1),
+                                    torch.cat([-_d,   _c,  -_b,    a], dim=1)], dim=0)
+        
+        if ctx.needs_input_grad[2] and bias is not None:
+            
+            grad_input_bias = grad_output.sum(0)
+                
+        return grad_input_r, grad_input_bias
+    
+    
+class QLinearAutograd(torch.autograd.Function):
+    
+    @staticmethod
+    def forward(ctx, input, weight, bias, device, type):
+
+        ctx.save_for_backward(input, weight.a, bias)
+        if bias is not None:
+            bias = bias.to(device)
+        output = getattr(F, type)(input, weight.torch().to(device), bias)
+        
+        return output
+       
+    @staticmethod
+    def backward(ctx, grad_output):
+        
+        grad_input_r = grad_input_bias = None
+        
+        input, weight_r, bias, = ctx.saved_tensors
+        grad_output_r = grad_output[:,:grad_output.size(1)//4]
+        
+        if ctx.needs_input_grad[0] and ctx.needs_input_grad[1]:
+            with torch.enable_grad():
+                
+                grad_input_r = torch.autograd.grad(output, weight_r, grad_output_r)
+            
+            a, _b, _c, _d = torch.chunk(grad_input_r, 0)
+            grad_input = torch.cat([torch.cat([  a,   _b,   _c,   _d], dim=1),
+                                    torch.cat([-_b,    a,   _d,  -_c], dim=1),
+                                    torch.cat([-_c,  -_d,    a,   _b], dim=1),
+                                    torch.cat([-_d,   _c,  -_b,    a], dim=1)], dim=0)
+        
+        if ctx.needs_input_grad[2] and bias is not None:
+            
+            grad_input_bias = grad_output.sum(0)
+                
+        return grad_input_r, grad_input_bias
