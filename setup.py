607b2dedb95ab97b86ce1b6389bbbbe0ae3632d9
giorgiozannini <giorgiozannini97@gmail.com>
2021-02-23 20:35:09 +0100

    small corrections.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="qtorch-giorgiozannini",
+    name="htorch-giorgiozannini",
     version="0.0.1",
-    author="Giorgio Zannini",
+    author="Giorgio Zannini Quirini",
     author_email="giorgiozannini97@gmail.com",
     description="PyTorch extension to support quaternion-valued tensors",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/giorgiozannini/QNN",
+    url="https://github.com/giorgiozannini/hTorch",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
