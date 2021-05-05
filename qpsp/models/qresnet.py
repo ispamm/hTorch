fdcf9f7ac6968af1ed4aa03c56d7dfe9334368e5
giorgiozannini <giorgiozannini97@gmail.com>
2021-05-05 19:56:41 +0200

    added swin and unet

--- qpsp/models/qresnet.py
+++ qpsp/models/qresnet.py
@@ -6,8 +6,15 @@ from htorch.quaternion import *
 from htorch.layers import QConv2d
 from htorch.functions import QModReLU
 
+def set_ops(quaternion)
+    global conv, act, factor
+    conv = QConv2d if quaternion else nn.Conv2d
+    act = QModReLU if quaternion else nn.ReLU
+    factor = 4
+
+
 def conv3x3(in_planes, out_planes, stride=1):
-    return QConv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+    return conv(in_planes, out_planes, kernel_size=3, stride=stride,
                    padding=1, bias=False)
 
 
@@ -18,21 +25,24 @@ class ChannelBlock(nn.Module):
     """
     def __init__(self, inplanes, planes, stride=1):
         super(ChannelBlock, self).__init__()
+
+        self.act = act()
+
         self.bn1 = nn.BatchNorm2d(inplanes * 4)
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride,
+        self.conv1 = conv(inplanes // factor, planes // factor, kernel_size=1, stride=stride,
                                padding=0, bias=False)
 
         self.bn2 = nn.BatchNorm2d(planes * 4)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=1, stride=stride,
+        self.conv2 = conv(planes // factor, planes // factor, kernel_size=1, stride=stride,
                                padding=0, bias=False)
 
         self.stride = stride
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
+        out = self.act(self.bn1(x))
         out = self.conv1(out)
 
-        out = F.relu(self.bn2(out))
+        out = self.act(self.bn2(out))
         out = self.conv2(out)
 
         return out
@@ -43,9 +53,9 @@ class BasicBlock(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
+        self.act = act()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes * 4)
-        self.mod_relu = QModReLU()
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes * 4)
         self.downsample = downsample
@@ -55,7 +65,7 @@ class BasicBlock(nn.Module):
         residual = x
 
         out = self.conv1(x)
-        out = F.relu(self.bn1(out))
+        out = self.act(self.bn1(out))
 
         out = self.conv2(out)
         out = self.bn2(out)
@@ -64,7 +74,7 @@ class BasicBlock(nn.Module):
             residual = self.downsample(x)
 
         out += residual
-        out = F.relu(out)
+        out = self.act(out)
 
         return out
 
@@ -74,12 +84,12 @@ class Bottleneck(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-        self.conv1 = QConv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.conv1 = conv(inplanes // factor, planes // factor, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes * 4)
-        self.conv2 = QConv2d(planes, planes, kernel_size=3, stride=stride,
+        self.conv2 = conv(planes // factor, planes // factor, kernel_size=3, stride=stride,
                              padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes * 4)
-        self.conv3 = QConv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.conv3 = conv(planes // factor, planes // factor * self.expansion, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * 4 * self.expansion)
         self.downsample = downsample
         self.stride = stride
@@ -88,10 +98,10 @@ class Bottleneck(nn.Module):
         residual = x
 
         out = self.conv1(x)
-        out = F.relu(self.bn1(out))
+        out = self.act(self.bn1(out))
 
         out = self.conv2(out)
-        out = F.relu(self.bn2(out))
+        out = self.act(self.bn2(out))
 
         out = self.conv3(out)
         out = self.bn3(out)
@@ -100,7 +110,7 @@ class Bottleneck(nn.Module):
             residual = self.downsample(x)
 
         out += residual
-        out = F.relu(out)
+        out = self.act(out)
 
         return out
 
@@ -109,32 +119,34 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=10, deep_base=True):
         super(ResNet, self).__init__()
+        
+        self.act = act()
         self.deep_base = deep_base
         if not self.deep_base:
-            self.inplanes = 64 // 4
-            self.conv1 = QConv2d(8 // 4, 64 // 4, kernel_size=7, stride=2, padding=3, bias=False)
+            self.inplanes = 64 // factor
+            self.conv1 = conv(8 // factor, 64 // factor, kernel_size=7, stride=2, padding=3, bias=False)
             self.bn1 = nn.BatchNorm2d(64)
         else:
-            self.inplanes = 128 // 4
-            self.conv1 = conv3x3(8 // 4, 64 // 4, stride=2)
+            self.inplanes = 128 // factor
+            self.conv1 = conv3x3(8 // factor, 64 // factor, stride=2)
             self.bn1 = nn.BatchNorm2d(64)
-            self.conv2 = conv3x3(64 // 4, 64 // 4)
+            self.conv2 = conv3x3(64 // factor, 64 // factor)
             self.bn2 = nn.BatchNorm2d(64)
-            self.conv3 = conv3x3(64 // 4, 128 // 4)
+            self.conv3 = conv3x3(64 // factor, 128 // factor)
             self.bn3 = nn.BatchNorm2d(128)
         self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64 // 4, layers[0])
-        self.layer2 = self._make_layer(block, 128 // 4, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256 // 4, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512 // 4, layers[3], stride=2)
+        self.layer1 = self._make_layer(block, 64 // factor, layers[0])
+        self.layer2 = self._make_layer(block, 128 // factor, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256 // factor, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512 // factor, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 // 4 * block.expansion, num_classes)
+        self.fc = nn.Linear(512 // factor * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                QConv2d(self.inplanes, planes * block.expansion,
+                conv(self.inplanes, planes * block.expansion,
                         kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes * 4 * block.expansion),
             )
@@ -148,10 +160,10 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.act(self.bn1(self.conv1(x)))
         if self.deep_base:
-            x = F.relu(self.bn2(self.conv2(x)))
-            x = F.relu(self.bn3(self.conv3(x)))
+            x = self.act(self.bn2(self.conv2(x)))
+            x = self.act(self.bn3(self.conv3(x)))
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -166,46 +178,57 @@ class ResNet(nn.Module):
         return x
 
 
-def resnet18(pretrained=False, **kwargs):
+def resnet18(pretrained=False, quaternion=True, **kwargs):
     """Constructs a ResNet-18 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
+    global conv
+    conv = QConv2d if quaternion
+
     model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
     return model
 
 
-def resnet34(pretrained=False, **kwargs):
+def resnet34(pretrained=False, quaternion=True, **kwargs):
     """Constructs a ResNet-34 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
+    global conv
+    conv = QConv2d if quaternion
+
     model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
     return model
 
 
-def resnet50(pretrained=False, **kwargs):
+def resnet50(pretrained=False,  quaternion=True, **kwargs):
     """Constructs a ResNet-50 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
+    global conv
+    conv = QConv2d if quaternion
+
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     if pretrained:
-        # model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
         model_path = './initmodel/resnet50_v2.pth'
         model.load_state_dict(torch.load(model_path), strict=False)
     return model
 
 
-def resnet101(pretrained=False, **kwargs):
+def resnet101(pretrained=False, quaternion=True, **kwargs):
     """Constructs a ResNet-101 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
+    global conv
+    conv = QConv2d if quaternion
+
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     if pretrained:
         model_path = './initmodel/resnet101_v2.pth'
@@ -213,14 +236,16 @@ def resnet101(pretrained=False, **kwargs):
     return model
 
 
-def resnet152(pretrained=False, **kwargs):
+def resnet152(pretrained=False, quaternion=True, **kwargs):
     """Constructs a ResNet-152 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
+    global conv
+    conv = QConv2d if quaternion
+
     model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
     if pretrained:
-        # model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
         model_path = './initmodel/resnet152_v2.pth'
         model.load_state_dict(torch.load(model_path), strict=False)
     return model
---
+++ qpsp/models/qunet.py
@@ -0,0 +1,116 @@
+"""
+credit: https://github.com/usuyama/pytorch-unet/blob/master/pytorch_unet.py
+"""
+
+import pytorch_lightning as pl
+from htorch.layers import QConv2d
+from htorch.functions import QModReLU
+import torch
+import torch.nn as nn
+
+def set_ops(quaternion)
+    global conv, act, factor
+    conv = QConv2d if quaternion else nn.Conv2d
+    act = QModReLU if quaternion else nn.ReLU
+    factor = 4
+
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        conv(in_channels, out_channels, 3, padding=1),
+        act(),
+        conv(out_channels, out_channels, 3, padding=1),
+        act()
+    )   
+
+
+class UNet(nn.Module):
+
+    def __init__(self, quaternion=True, n_class=10):
+        super().__init__()
+
+        set_ops(quaternion)
+
+        self.dconv_down1 = double_conv(3, 64 // factor)
+        self.dconv_down2 = double_conv(64 // factor, 128 // factor)
+        self.dconv_down3 = double_conv(128 // factor, 256 // factor)
+        self.dconv_down4 = double_conv(256 // factor, 512 // factor)        
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
+        
+        self.dconv_up3 = double_conv((256 + 512) // factor, 256 // factor)
+        self.dconv_up2 = double_conv((128 + 256) // factor, 128 // factor)
+        self.dconv_up1 = double_conv((128 + 64) // factor, 64 // factor)
+        
+        self.conv_last = nn.Conv2d(64, n_class, 1)
+        
+        
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+        
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)   
+        
+        x = self.dconv_down4(x)
+        
+        x = self.upsample(x)        
+        x = torch.cat([x, conv3], dim=1)
+        
+        x = self.dconv_up3(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, conv2], dim=1)       
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, conv1], dim=1)   
+        
+        x = self.dconv_up1(x)
+        
+        out = self.conv_last(x)
+        
+        return out
+    
+    def configure_optimizers(self):
+        optimizer = MADGRAD(self.parameters(), lr=LEARNING_RATE)
+        return optimizer
+
+    def focal_tversky_loss(self, x, y):
+        loss = FocalTverskyLoss()(x, y)
+        return loss
+
+    def training_step(self, train_batch, batch_idx):
+        inputs, labels = train_batch
+        outputs = self.forward(inputs, labels) 
+
+        probs = torch.sigmoid(outputs).data.cpu().numpy()
+        crf = np.stack(list(map(dense_crf_wrapper, zip(inputs.cpu().numpy(), probs))))
+        crf = np.ascontiguousarray(crf)
+        f1_crf = f1_score(torch.from_numpy(crf).to(self.device), labels)
+
+        loss =  self.focal_tversky_loss(outputs.float(), labels.float())
+        f1 = f1_score(outputs, labels)
+
+        self.log('train_loss', loss)
+        self.log('train_f1', f1)
+        self.log('train_f1_crf', f1_crf)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        inputs, labels = val_batch
+        outputs = self.forward(inputs, labels)
+
+        probs = torch.sigmoid(outputs).data.cpu().numpy()
+        crf = np.stack(list(map(dense_crf_wrapper, zip(inputs.cpu().numpy(), probs))))
+        crf = np.ascontiguousarray(crf)
+        f1_crf = f1_score(torch.from_numpy(crf).to(self.device), labels)
+
+        loss = self.focal_tversky_loss(outputs.float(), labels.float())
+        f1 = f1_score(outputs, labels)
+
+        self.log('val_loss', loss)
+        self.log('val_f1_crf', f1_crf)
+        self.log('val_f1', f1)
